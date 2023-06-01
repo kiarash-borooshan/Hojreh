99d6fd3ecddf76deae1841417e483d04c676f2bc
kia <K@b.com>
2023-06-01 10:36:26 +0330

    phonenumber_field does not recognize profile and receiver initiated

--- Hojerh/account/models.py
+++ Hojerh/account/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
-"""" cannot recogniza """
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+"""" cannot recognize """
 # from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -11,3 +14,16 @@ class Profile(models.Model):
     # phone_number = PhoneNumberField(blank=True)
     phone_number = models.CharField(blank=True, null=True)
     photo = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user}"
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    user = kwargs.get('instance')
+    created = kwargs.get('created')
+
+    if created:
+        profile = Profile(user=user)
+        profile.save()
