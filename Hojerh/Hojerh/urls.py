1affbbaef43bf63d0376ce8782403756edfb3938
kia <K@b.com>
2023-06-01 11:45:10 +0330

    forms.py initiated for account app

--- Hojerh/Hojerh/urls.py
+++ Hojerh/Hojerh/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('core.url', namespace='core'))
+    path("", include('core.url', namespace='core')),
+    path("auth/", include('account.url')),
 ]
