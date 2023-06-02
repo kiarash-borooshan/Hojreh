28cda1876176ee81a1c7dc1730e790863efc6a54
kia <K@b.com>
2023-06-02 17:56:01 +0330

    hash password set

--- Hojerh/account/views.py
+++ Hojerh/account/views.py
@@ -1,9 +1,27 @@
+from django.contrib.auth.models import User
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .forms import RegisterForm
 
 
-def register_user(request):
-    form = RegisterForm()
+def register_user(request: HttpRequest):
+
+    if request.method == "POST":
+
+        """ register user """
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            user: User = form.save(commit=False)
+            user.set_password(cd['password'])
+            user.save()
+
+            return HttpResponse("Created account")
+
+    else:
+        form = RegisterForm()
+
     return render(request,
                   'account/register.html',
                   {'form': form})
