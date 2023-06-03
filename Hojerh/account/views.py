8e84cc65a3f93578e682f281d3d80b8cf00c1607
kia <K@b.com>
2023-06-03 10:29:04 +0330

    profile form added to register page

--- Hojerh/account/views.py
+++ Hojerh/account/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .forms import RegisterForm, RegisterProfileForm
+from .models import Profile
 
 
 def register_user(request: HttpRequest):
@@ -10,13 +11,21 @@ def register_user(request: HttpRequest):
 
         """ register user """
         form = RegisterForm(request.POST)
-        profile_form = RegisterProfileForm(request.POST)
+        profile_form = RegisterProfileForm(request.POST, files=request.FILES)
 
-        if form.is_valid():
+        if form.is_valid() and profile_form.is_valid():
+
             cd = form.cleaned_data
+            pcd = profile_form.cleaned_data
+
             user: User = form.save(commit=False)
             user.set_password(cd['password'])
             user.save()
+
+            profile = Profile.objects.get(user=user)
+            profile.photo = pcd['photo']
+            profile.phone_number = pcd['phone_number']
+            profile.save()
 
             return HttpResponse("Created account")
 
