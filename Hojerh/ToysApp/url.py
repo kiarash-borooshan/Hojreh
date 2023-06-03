d836f16c7eeeb05aa16da8c0fe930b35b89f573c
kia <K@b.com>
2023-06-03 19:07:07 +0330

    parial

--- Hojerh/ToysApp/url.py
+++ Hojerh/ToysApp/url.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = 'ToysApp'
 
 urlpatterns = [
-    path("DonyayeKoodakan/", views.index_toys)
+    path("donyaye koodakan/", views.index_toys, name="index"),
+    path("donyaye koodakan detail/", views.detail_toys, name='detail'),
+    path("donyaye koodakan categories/", views.categories_toys, name='categories')
 ]
--- Hojerh/ToysApp/views.py
+++ Hojerh/ToysApp/views.py
@@ -5,3 +5,15 @@ def index_toys(request):
 
     return render(request,
                   'ToysStore/ToysIndex.html')
+
+
+def detail_toys(request):
+
+    return render(request,
+                  'ToysStore/ToysDetail.html')
+
+
+def categories_toys(request):
+
+    return render(request,
+                  'ToysStore/ToysCategories.html')
