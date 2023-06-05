c6740865b9b58121c7f30251be0e0514292b9c7d
kia <K@b.com>
2023-06-05 22:32:57 +0330

    pagination completed

--- Hojerh/ToysApp/views.py
+++ Hojerh/ToysApp/views.py
@@ -1,17 +1,30 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Toys, Category, Ages, GenderType
 
 
 def index_toys(request):
 
-    toy = Toys.objects.all().order_by("-date")
-    # toy = Toys.objects.filter(available=True).order_by("-update")
+    o_l = Toys.objects.all().order_by("-date")
+    # o_l = Toys.objects.filter(available=True).order_by("-update")
 
     cat = Category.objects.all()
 
     gender_type = GenderType.objects.all()
 
     age = Ages.objects.all()
+
+    """" pagination """
+    page = request.GET.get('page')
+    paginator = Paginator(o_l, 3)
+
+    try:
+        toy = paginator.page(page)
+    except EmptyPage:
+        toy = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        toy = paginator.page(1)
+
 
     context = {"toys": toy,
                "cats": cat,
