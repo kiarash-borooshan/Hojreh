7500c06d7131c6fe937fa2981678e7fcdc921989
kia <K@b.com>
2023-06-12 11:30:49 +0330

    costume theme

--- Hojerh/account/models.py
+++ Hojerh/account/models.py
@@ -8,12 +8,21 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Profile(models.Model):
+    Theme_CHOICES = (
+        ('is-light', 'Light'),
+        ('is-success', "Green"),
+        ("is-warning", "Yellow"),
+        ("is-info", "Blue"),
+        ("is-dark", "Dark")
+    )
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
                                 related_name="profile")
     phone_number = PhoneNumberField(blank=True, null=True)
     # phone_number = models.CharField(blank=True, null=True)
     photo = models.ImageField(null=True, blank=True)
+    theme = models.CharField(max_length=50,
+                             choices=Theme_CHOICES, default="is-success")
 
     def __str__(self):
         return f"{self.user}"
